c52da7554da36eac85449e5aac12cfbb9ced880e
jonngyeon <whddus0426@naver.com>
2023-01-11 21:29:27 +0900

    1/11

---
+++ src/Tv만들기/TvMain.java
@@ -0,0 +1,14 @@
+package Tv만들기;
+
+public class TvMain {
+    public static void main(String[] args) {
+        TvClass samsungTv = new TvClass();
+        samsungTv.getTV();
+        TvClass lgTv = new TvClass(true, 12, 20);
+        lgTv.getTV();
+        lgTv.setChannel(1000);
+        lgTv.setVolume(90);
+        lgTv.getTV();
+
+    }
+}
