99f59c7e624985ec4386c681d0c6765a12555219
jonngyeon <whddus0426@naver.com>
2023-01-17 21:30:20 +0900

    1/17

---
+++ src/은행만들기/BankClass.java
@@ -0,0 +1,29 @@
+package 은행만들기;
+public class BankClass {
+    private static int cnt = 0; // 같은 클래스 내에서 접근 가능한 정적 변수(클래스 변수)
+    private int account; // 계좌
+    private String bank; // 은행의 이름
+    public BankClass(String name, int account) {
+        cnt++; // 클래스가 객체로 만들어 질 때 값이 증가 됨
+        this.bank = name;
+        this.account = account;
+        System.out.println(name + "은행에 계좌를 개설 합니다. 잔액은 " + account + account + "입니다.");
+    }
+    public static int getCnt() { // 정적 메소드
+        return cnt;
+    }
+    public void setDeposit(final int dep) {
+        this.account += dep;
+        System.out.println(dep + "을 예금 했습니다.");
+    }
+    public void setWithdraw(final int with) {
+        if(with > account) {
+            System.out.println("잔액이 부족 합니다");
+        } else {
+            account -= with;
+        }
+    }
+    public void viewAccount() {
+        System.out.println("현재 잔액은 " + account + "입니다.");
+    }
+}
